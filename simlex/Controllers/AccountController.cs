af6696753b81eb8d39da02db181dd870bf2a6419
simlex-titul2005 <simlex.dev.2014@gmail.com>
2017-01-09 15:18:16 +0200

    2.4

--- simlex/Controllers/AccountController.cs
+++ simlex/Controllers/AccountController.cs
@@ -1,9 +1,15 @@
-﻿using SX.WebCore.MvcControllers;
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using SX.WebCore.MvcControllers;
+using SX.WebCore.ViewModels;
 
 namespace simlex.Controllers
 {
     public sealed class AccountController : SxAccountController
     {
-        
+        public override Task<ActionResult> Login(SxVMLogin model, string returnUrl)
+        {
+            return base.Login(model, Url.Action("List", "Articles"));
+        }
     }
 }
